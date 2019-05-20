99ffb2e48a62f662599a1005dd788d8d085ce5d1
kmhosny <kmhosny@yahoo.com>
2019-05-20 15:05:19 +0200

    scaffold app

---
+++ src/main/java/com/b2bf/b2bf/B2bfApplication.java
@@ -0,0 +1,13 @@
+package com.b2bf.b2bf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b2bf/b2bf/B2bfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.b2bf.b2bf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class B2bfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
