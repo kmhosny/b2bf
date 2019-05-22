ed2715d24cd4736f6b332b189fdb468c047df959
kmhosny <kmhosny@yahoo.com>
2019-05-22 16:07:59 +0200

    change package structure

--- src/main/java/b2bf/FlagRepository.java
+++
@@ -1,7 +0,0 @@
-package b2bf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlagRepository extends JpaRepository<Flag, Integer> {
-
-}
--- src/main/java/b2bf/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package b2bf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
